3d2bfa14067f456008ac5c958a464891dd206863
Evan Fader <efader@brandeis.edu>
2014-03-07 17:22:56 -0500

    generating dummy data, but need single table inheritance

--- src/app/controllers/configurator_controller.rb
+++ src/app/controllers/configurator_controller.rb
@@ -1,6 +1,12 @@
 class ConfiguratorController < ApplicationController
-	def index
+	#before_action :generate, only: index end
 
+	def index
+		self.generate
+		wick = Product.find(1)
+
+
+		drop_all
 	end
 
 	def is_compatible(part)
@@ -21,11 +27,19 @@ class ConfiguratorController < ApplicationController
 
 	def generate
 		(1..10).each do |wick|
-			Product.create(type:"wick",name:("wick" + wick.to_s))
+			Product.create(type:"wick",name:("wick" + wick.to_s),wick_id:wick.to_s)
 		end
+
 		(1..10).each do |tank|
-			Product.create(type:"tank",name:("tank" + tank.to_s))
+			Product.create(type:"tank",name:("tank" + tank.to_s),tank_id:tank.to_s)
 		end
+	end
+ 
+	def drop_all
+		Product.all.each do |product|
+			product.destroy
+		end
+
 	end
 
 end
--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	validates :type, :presence => true
 	validates :name, :presence => true
+	
 end
