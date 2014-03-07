6883ecf75ad997af30651a849b4b9f2c9b33072f
Evan Fader <efader@brandeis.edu>
2014-03-07 13:52:56 -0500

    basic structure

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,8 @@
 Src::Application.routes.draw do
   get "main/configuration"
   get "main/search"
+
+  get "/configurator" => "configurator#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
