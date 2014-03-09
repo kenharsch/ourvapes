decc4fb8e59151bb200f0181746868a87913b7d2
Konwisser <gekonwi@brandeis.edu>
2014-03-08 20:34:18 -0500

    created inheriting classes; db/seeds.rb stores a Wick, a Tank, a Kit, and a Product object in the db; URL/products demonstrates ID and type access - both set automatically by rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Src::Application.routes.draw do
+  resources :products
+
   get "main/configuration"
   get "main/search"
   # The priority is based upon order of creation: first created -> highest priority.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tank.create(title: 'Super Tank 1', descripton: 'a really super tank')
+Wick.create(title: 'Super Wink 2', descripton: 'a really super wink')
+Kit.create(title: 'Super Kit 3', descripton: 'a really super kit')
+
+Product.create(title: 'Super Product 4', descripton: 'a really super product')
