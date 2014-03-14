c23dc9fd2e82a40bf92ec2e1a16915b9866aa2cc
Konwisser <gekonwi@brandeis.edu>
2014-03-14 00:11:19 -0400

    refactored Battery test [#65910972]

--- test/models/battery_test.rb
+++ test/models/battery_test.rb
@@ -3,24 +3,42 @@ require 'test_helper'
 class BatteryTest < ActiveSupport::TestCase
 	describe "battery" do
 		before do
-			@bat_count_before = Battery.count
+			@bat_count_before = Product.count
 			@det_count_before = BatteryDet.count
+			@b = Battery.create
 		end
 
-		it "creates no BatteryDet object if not needed" do
-			b = Battery.create
-			b.name = "Super Battery"
-			b.description = "really cool"
-			Battery.count.must_equal (@bat_count_before + 1)
+		it "creates no details object if not needed" do
+			@b.name = "Super Battery"
+			@b.description = "really cool"
+			Product.count.must_equal (@bat_count_before + 1)
 			BatteryDet.count.must_equal (@det_count_before)
 		end
 
-		it "creates and associates a BatteryDet object if needed" do
-			b = Battery.first
-			b.voltage = 12
-			Battery.count.must_equal (@bat_count_before + 1)
-			BatteryDet.count.must_equal (@det_count_before + 1)
-		end
+		describe "interacts with associated details object" do
 
+			before do
+				@b.voltage = 12
+				@b.save
+			end
+
+			it "creates and associates a details object if needed" do
+				Product.count.must_equal (@bat_count_before + 1)
+				BatteryDet.count.must_equal (@det_count_before + 1)
+			end
+
+			it "deletes associated details object if host object gets deleted" do
+				@b.delete
+				Product.count.must_equal @bat_count_before
+				BatteryDet.count.must_equal @det_count_before
+			end
+
+			it "destroys associated details object if host object gets deleted" do
+				@b.destroy
+				Product.count.must_equal @bat_count_before
+				BatteryDet.count.must_equal @det_count_before
+			end
+
+		end
 	end
 end
