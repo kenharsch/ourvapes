77265eb8063cdf1fb9cf63224e36a497a249cb36
Steven H <stevenhh@brandeis.edu>
2014-03-19 20:35:24 -0400

    stole some of Evan's code

--- app/controllers/product_lists_controller.rb
+++ app/controllers/product_lists_controller.rb
@@ -1,10 +1,38 @@
 class ProductListsController < ApplicationController
-  before_action :set_product_list, only: [:show, :edit, :update, :destroy]
+  #before_action :set_product_list, only: [:show, :edit, :update, :destroy]
 
   # GET /product_lists
   # GET /product_lists.json
   def index
-    @product_lists = Product.all
+    @add_or_change = "Add"
+
+    #populate_database(100)
+    #empty_database
+
+    @newprod = []
+    Product.all.each do |prod|
+      @newprod << [prod.name, prod.id]
+    end
+
+    find_parts
+  end
+
+    def find_parts
+    @partslist = ["mouthpiece","tank","wick","button","battery","charger"]
+    @kit = {}
+    params["kit"] ||= {}
+
+    params["kit"].each do |part_type, part_id|
+      model = part_type.capitalize.constantize
+      if model.exists?(part_id)
+        @kit[part_type] = model.find(part_id).name
+      else
+        flash[:notice] = "Invalid "+ part_type +" selection"
+        params["kit"].delete(params["kit"].index(part_type))
+      end
+    end
+
+
   end
 
   # GET /product_lists/1
