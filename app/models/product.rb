cb6dad9aae122aaedc831b21169ea0755a7997ca
Evan Fader <efader@brandeis.edu>
2014-03-20 14:57:58 -0400

    repaired dbhelper, added methods to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -60,7 +60,7 @@ class Product < ActiveRecord::Base
 
 	def self.are_compat?(prod1, prod2)
 		prod1, prod2 = prod2, prod1 if prod2.id < prod1.id
-		CompatPair.find_by prod1_id: prod1.id, prod2_id: prod2.id
+		return !CompatPair.find_by(prod1_id: prod1.id, prod2_id: prod2.id).nil?
 	end
 
 	def self.set_compat(prod1, prod2)
