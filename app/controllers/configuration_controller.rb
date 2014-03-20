089e3a4f80e51168b7624ae71a84bd4bac32e8fb
Evan Fader <efader@brandeis.edu>
2014-03-19 20:54:04 -0400

    randomized adding

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -17,35 +17,36 @@ class ConfigurationController < ApplicationController
 
 
 
-	def add
-		@data = params["data"]
-		conf = {tank: 70}
-		hash = {conf: conf}
-		redirect_to "/configuration?#{hash.to_param}"
-	end
-
-	def remove
+	def add(add_type = params["add_type"], add_id = params["add_id"])
 		conf = params["conf"] || {}
-		conf.delete(params["remove"])
-		hash = {conf: conf}
-		redirect_to "/configuration?#{hash.to_param}"
-
+		conf[add_type] = add_id
+		go_to_config(conf)
 	end
 
+	def remove(remove_item = params["remove"])
+		conf = params["conf"] || {}
+		conf.delete(remove_item)
+		go_to_config(conf)
+	end
+
+	def go_to_config(conf = {})
+		hash = {conf: conf}
+		redirect_to "/configuration?#{hash.to_param}"
+	end
 
 
 	def find_parts
 		@partslist = ["mouthpiece","tank","wick","button","battery","charger"]
-		@conf = {}
+		@conf_objects = {}
 		params["conf"] ||= {}
 
 		params["conf"].each do |part_type, part_id|
-			model = part_type.capitalize.constantize
-			if model.exists?(part_id)
-				@conf[part_type] = model.find(part_id).name
+			part_class = part_type.capitalize.constantize
+			if part_class.exists?(part_id)
+				@conf_objects[part_type] = part_class.find(part_id)
 			else
-				flash[:notice] = "Invalid "+ part_type +" selection"
-				params["conf"].delete(params["conf"].index(part_type))
+				flash[:notice] = "Invalid " + part_type + " selection"
+				remove(part_type)
 			end
 		end
 
