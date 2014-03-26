0c4adfe4fc56e54a5c59b91b461c9dcb653f57e2
Evan Fader <efader@brandeis.edu>
2014-03-26 19:08:17 -0400

    better errors

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,30 +11,30 @@ class ProductTest < ActiveSupport::TestCase
 
 		describe "'compatible to' statement" do
 			it "is false if two products were not set as compatible before" do
-				@p1.compat_to?(@p2).must_equal false
-				@p2.compat_to?(@p1).must_equal false
+				@p1.compat_with?(@p2).must_equal false
+				@p2.compat_with?(@p1).must_equal false
 			end
 
 			it "is true if two products were set as compatible before" do
-				@p1.set_compat_to(@p2, true)
+				@p1.set_compat_with(@p2, true)
 
-				@p1.compat_to?(@p2).must_equal true
-				@p2.compat_to?(@p1).must_equal true
+				@p1.compat_with?(@p2).must_equal true
+				@p2.compat_with?(@p1).must_equal true
 			end
 
 			it "can be changed afterwads" do
-				@p1.set_compat_to(@p2, true)
-				@p1.set_compat_to(@p2, false)
+				@p1.set_compat_with(@p2, true)
+				@p1.set_compat_with(@p2, false)
 
-				@p1.compat_to?(@p2).must_equal false
-				@p2.compat_to?(@p1).must_equal false
+				@p1.compat_with?(@p2).must_equal false
+				@p2.compat_with?(@p1).must_equal false
 			end
 
 			it "can be set to 'false' right at the beginning" do
-				@p1.set_compat_to(@p2, false)
+				@p1.set_compat_with(@p2, false)
 
-				@p1.compat_to?(@p2).must_equal false
-				@p2.compat_to?(@p1).must_equal false
+				@p1.compat_with?(@p2).must_equal false
+				@p2.compat_with?(@p1).must_equal false
 			end
 		end
 
@@ -45,7 +45,7 @@ class ProductTest < ActiveSupport::TestCase
 			end
 
 			it "is false if the products are compatible but not defined as 'working well'" do
-				@p1.set_compat_to(@p2, true)
+				@p1.set_compat_with(@p2, true)
 
 				@p1.works_well_with?(@p2).must_equal false
 				@p2.works_well_with?(@p1).must_equal false
@@ -76,8 +76,8 @@ class ProductTest < ActiveSupport::TestCase
 			it "subsums the 'compatible to' statement" do
 				@p1.set_works_well_with(@p2, true)
 
-				@p1.compat_to?(@p2).must_equal true
-				@p2.compat_to?(@p1).must_equal true
+				@p1.compat_with?(@p2).must_equal true
+				@p2.compat_with?(@p1).must_equal true
 			end
 		end
 	end
