ffd2d48998e1e9dd7399cbddf33c0087ead7bba2
Georg Konwisser <gekonwi@gmail.com>
2014-03-30 13:47:43 -0400

    [Fixes #68495672]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,49 @@
+LOREM = 'Veniam admodum posidonium quo ad, et copiosae recusabo sea, at corrumpit voluptatibus vel. Modo congue ea has, his cu purto appetere. Ut nec affert docendi alienum, te erant mundi vix, vix ex novum officiis evertitur. Possit dissentiunt ea pro. In duo tantas viderer reprehendunt, eu diceret placerat mei.
+
+Est fuisset inimicus adolescens et, tale adipisci adolescens eos an. Ei ius autem admodum, ex per omnes antiopam neglegentur. Et ridens appetere pri, pertinax percipitur mea in, doctus repudiandae te his. Imperdiet torquatos ei pri.
+
+Per ei velit mazim incorrupte, vis fugit vidisse an, no mei oblique ornatus. Ea principes ullamcorper duo, id nec dicant vidisse voluptatibus, in laoreet docendi elaboraret vix. Ne nostrud maluisset persequeris ius. Sit nihil perpetua ei, eam commodo malorum corrumpit ex, nec te utamur adipisci.
+
+Porro menandri torquatos eu qui, ius ea quaerendum concludaturque. No his atqui velit facilis. Sed et omnis repudiandae, quo et hinc munere principes, vocibus salutatus signiferumque an qui. Eu esse postulant his, ius in viris epicuri quaerendum, nusquam suscipit scripserit eu cum. Numquam utroque mediocritatem duo id.
+
+Vis ut percipit electram forensibus, an expetendis liberavisse disputationi qui. Verterem salutatus id eum, diam abhorreant pro in. Vim melius blandit an, aeterno erroribus eam eu. Sed ludus electram repudiare ad, quo primis nostrum in. Accusam inciderint concludaturque sea ei.
+
+Aeque inciderint his cu, ei saepe intellegam pri, putent quodsi quo at. Nec feugait molestiae voluptatibus cu, deleniti maluisset disputationi vel ad. Eos id facete scriptorem, postea facete causae no eam. Aliquip pericula mea ut, sit an omnium iriure latine, his et gubergren voluptatibus. An vim mediocrem consectetuer. Duo ne autem dicat facer.
+
+In est minimum omittam abhorreant, eos in consul accumsan. Vim te inani meliore. Et scripta diceret inciderint usu, sit mundi munere ea, est illud commodo numquam cu. Veniam explicari no nam, duo ea tota splendide, ei sea laudem possit virtute.
+
+Ea eum accumsan probatus appellantur. Brute commune qui ex, saperet fierent suscipiantur ne duo, numquam insolens delicata vel in. An sea justo saperet maiorum, eos ea partiendo imperdiet, eu pri laudem melius partiendo. Accusam menandri an his, ad qui probo persius, mel at ocurreret aliquando. Eu cum mollis fastidii, eos dolor ornatus fabellas cu.
+
+Homero accommodare nam an, eam at aperiri singulis. Eu vis debet libris delectus, mutat vitae eam cu, te mutat instructior his. Tempor cetero elaboraret vix id, ea usu zril dissentias. Tamquam phaedrum eu nam. Ut probo solum epicuri vix, ne quem tale tincidunt mea.
+
+Sit maiorum percipit adipiscing eu, te oblique volumus vix. Te sit justo simul efficiendi. Case maluisset vituperata at ius, ut cum eros tantas. Lucilius appellantur in vel. His ei noster forensibus. Veniam petentium vim in.
+
+Nominati persequeris ut vix, no homero persius nonumes sed. Id essent dolorem repudiandae vim, an omittam fastidii platonem vim. Ut vis eripuit appareat suavitate. Splendide dissentiet ne ius, graece iriure legimus in sit. Vitae volumus rationibus an mea, te alia electram cum. Ea nostrum iudicabit nam, ut est sale sadipscing.
+
+Ad vim putant tamquam definitiones, quo id doctus molestie. Meis bonorum mei cu, nec debet prompta te. Et nec accumsan liberavisse. Purto theophrastus vim an, sit ut esse falli possim.
+
+Vocibus vivendo molestie pri in, fuisset nominati facilisis mei no. Qui ne quodsi consulatu ullamcorper. Ea epicuri forensibus necessitatibus qui, et pro phaedrum recusabo. Ut est integre iudicabit persequeris, verterem perfecto recusabo ne sea, et his quem habemus civibus. Omnes copiosae ut eos.
+
+An vocibus consulatu vim, alii tation tibique no eos. Mea te vocent aliquip eruditi, ut has regione aperiri repudiare, clita commodo eu eos. Putent bonorum in per. Nam viris tritani efficiendi in. Pro fugit aeque partem te, clita timeam est te, ad duo mazim corrumpit dissentias. Ad eam dolor nonumy.
+
+Homero pericula adipiscing et est. Aperiri inermis qui ut, nec amet fabulas no, pri ut agam regione assueverit. Affert sententiae eum no. Illum delicata ius cu, ea regione mandamus constituam vel. Ius eu harum eligendi, patrioque assentior et vim. Est omnes utinam adipisci an, pri ne singulis perpetua.
+
+Duo ex nisl dicunt. Id ferri ponderum periculis has, cu eum facer nominavi disputationi. Sed erat putant cu. Moderatius interesset nec id, nam in diam quodsi. Te hinc eius pri, discere deseruisse et vix, at vim tacimates accusamus. Dicta menandri instructior has at, vim et malis novum invenire, ad sint deterruisset reprehendunt sea.
+
+Hendrerit posidonium ei sed, id sed delectus posidonium. Ne vel nostrum ullamcorper, in cetero scripta mandamus mei. Eam iriure oportere te, sit unum docendi et, et ius enim putant. Vim ad veritus mediocrem referrentur. Convenire iracundia nam ex, pri iudico accumsan pericula ad. Pri porro equidem dissentias ea.
+
+Tale possit no pri, has ne movet docendi, debet inimicus conceptam cum an. Ne quis iisque verterem has. Meis vidit at mel, ei altera constituto sed. Dico tibique perpetua cu vix, sea diam omnis errem te, falli dolores principes an vis.
+
+Eu nec apeirian aliquando. Putant dissentiet repudiandae pro et, eos id eirmod docendi adipiscing, novum aperiam salutatus ut eos. Nostro perfecto partiendo id mel. Eam vivendum convenire ut, ullum accusata mea ei, atqui repudiare contentiones ad per. Nibh labore quidam vis cu, nec magna tritani an.
+
+Ut mel eros labitur numquam. Periculis adolescens ei quo, hinc erat pro ea. Mei nobis tacimates oportere eu, modo alia utamur est id. Ut his populo scripta. Ferri atqui placerat has no, sed salutandi salutatus contentiones ut. Ut mea legendos liberavisse. Vis labores ceteros id.
+
+Ex vel cibo appareat interesset, erant exerci admodum eum ex, ei ullum graeco quo. Ex ius appetere erroribus concludaturque. Ex pro mutat dolore audiam. No liber interesset mel. Mandamus perpetua ea qui, vix id omnium legendos. Inermis scribentur ius ne, quidam fuisset te vim.
+
+Ei nulla omnesque vix, vel in dolorum minimum. Dico consequuntur at usu, per quot constituto no. Ut has dolor dicam convenire, cu nostrum delicata pro. Pro at graecis atomorum delicatissimi, te est facete neglegentur. No stet iudico nusquam vim, eos virtute mandamus ex. His ne ipsum facilis fastidii.
+
+An pro clita patrioque theophrastus, novum facete his ad. An alii liberavisse vix.'
+
 companies = ["Aro", "Smok Tech", "Oddy", "EHPro", "Nimbus", "Aspire", "Kanger", "AmeraVape", "Innokin iClear", "Tugboat", "AnyVape", "Vision", "Omega", "Patriot", "Kayfun"]
 colors = ["Black", "Grey", "Stainless Steel", "Red", "Green", "Mystic Blue", "Forest Green", "Blue", "White", "Pearl White", "Metalic Red", "Fire Red", "Orange", "Hyper Yellow", "Pitch Black", "Pink", "Silver", "Gunmetal"]
 adjectives = ["Mini", "Large", "Flashy", "Twisted", "BDC", "RDX", "Time", "Cool", "Mod", "Pro", "Professional", "RDA", "Pyrex", "EVOD", "Dual", "Mega", "Micro", "Metal", "Super", "Power", "Vivi", "Victory", "Nova"]
@@ -11,9 +57,8 @@ mouth_pic = ['mouth1.jpg', 'mouth2.jpg', 'mouth3.jpg', 'mouth4.jpg', 'mouth5.jpg
 tank_pic = ['tank1.png', 'tank2.png', 'tank3.png', 'tank4.png', 'tank5.png']
 juice_pic = ['apple.png', 'banana.png', 'blueberry.png', 'cherry.png', 'coconut.png']
 descr = ['Perfect for the user that is committed to e-cigs and wants a low cost way to replace their ',
-		'Essential hardware for the vaping enthusiast. Compare this model with our competitors and you will that this is best ',
-		'The latest in vaping technology can be in your hand today! Check out the revolutionary technology in our ']
-
+	'Essential hardware for the vaping enthusiast. Compare this model with our competitors and you will that this is best ',
+	'The latest in vaping technology can be in your hand today! Check out the revolutionary technology in our ']
 
 for i in 0..16
 	wick = Wick.create(name: colors.sample + " " + adjectives.sample + " Wick", description: descr.sample + "Wick!")
@@ -65,6 +110,7 @@ end
 Product.all.each do |p|
 	p.manufacturer = companies.sample
 	p.price = (Random.rand(500...3500) / 100.0)
+	p.description += " \n\n#{LOREM}"
 	p.save
 end
 
