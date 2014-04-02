1a41ca58258723ba6b4ad3df72543bf5f7f77de5
Georg Konwisser <gekonwi@gmail.com>
2014-04-02 12:38:03 -0400

    we have a search now! :D wired up the search box with the fulltext search functionality

--- app/controllers/product_list_controller.rb
+++ app/controllers/product_list_controller.rb
@@ -1,46 +1,27 @@
 class ProductListController < ApplicationController
+
 	def list
 		@page = Constants::PAGE_PROD_LIST
 
 		@add_or_change = "Add"
 
-		# limit to 20 items for faster loading during development;
-		# exclude kits because we are not handling them als products yet
-		@prods = Product.where.not(type: "Kit").take(20)
+		@query = params["query"]
+
+		if @query.nil?
+			# limit to 20 items for faster loading during development;
+			# exclude kits because we are not handling them als products yet
+			@prods = Product.where.not(type: "Kit").take(20)
+		else
+			# for some reason the fulltext search does not work with @query
+			query = @query
+
+			search = Product.search do
+				fulltext query
+			end
+
+			@prods = search.results
+		end
 
 		@conf_objects = params["conf"] || {}
-		#find_parts
 	end
-
-=begin
-	def find_parts
-		@partslist = ["mouthpiece","tank","wick","button","battery","charger"]
-		@conf_objects = {}
-		params["conf"] ||= {}
-
-		params["conf"].each do |part_type, part_id|
-			part_class = part_type.capitalize.constantize
-			if part_class.exists?(part_id)
-				@conf_objects[part_type] = part_class.find(part_id)
-			else
-				flash[:notice] = "Invalid " + part_type + " selection"
-				remove(part_type)
-			end
-		end
-	end
-
-
-	def add(add_type = params["add_type"], add_id = params["add_id"])
-		conf = params["conf"] || {}
-		conf[add_type] = add_id
-		go_to_config(conf)
-	end
-
-	def remove(remove_item = params["remove"])
-		conf = params["conf"] || {}
-		conf.delete(remove_item)
-		go_to_config(conf)
-	end
-=end
-
 end
