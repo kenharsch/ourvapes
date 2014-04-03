782e7ad296c8e8e96cb426241e86f0f92e40310c
Georg Konwisser <gekonwi@gmail.com>
2014-04-03 01:37:47 -0400

    fixed: search killed type filter added: displaying the category in which VapeRater searched

--- app/controllers/product_list_controller.rb
+++ app/controllers/product_list_controller.rb
@@ -4,21 +4,25 @@ class ProductListController < ApplicationController
 		@page = Constants::PAGE_PROD_LIST
 		@add_or_change = "Add"
 
-		part_type_filter = params["type"]
+		@params_type = params["type"]
+		type_filter = @params_type
 
-		if part_type_filter.nil?
+		if type_filter.nil?
 			@title = "All Products"
 			@manus = Product.where.not(type: "Kit").select("manufacturer").distinct.order("manufacturer")
 		else
-			part_type_filter = part_type_filter.capitalize
-			@title = part_type_filter.pluralize
-			@manus = Product.where(type: part_type_filter).select("manufacturer").distinct.order("manufacturer")
+			type_filter = type_filter.capitalize
+			@title = type_filter.pluralize
+			@manus = Product.where(type: type_filter).select("manufacturer").distinct.order("manufacturer")
 		end
 
 		@query = params["query"]
-		@prods = ProdSearch.full_text(@query, part_type_filter, params[:page]).results
+		@prods = ProdSearch.full_text(@query, type_filter, params[:page]).results
 
-		@title = "Search Results" unless @query.nil?
+		unless @query.nil?
+			@searched_in = @title
+			@title = "Search Results"
+		end
 
 		@conf_objects = get_conf_objects
 	end
