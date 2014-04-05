0174035cb1076f3590bdcf0213e5374926e00a52
Georg Konwisser <gekonwi@gmail.com>
2014-04-05 13:23:19 -0400

    fixed all tests including the old css error it was a bootstrap problem - I deleted all local bootstrap files and switched to the CDN links

--- test/controllers/product_list_controller_test.rb
+++ test/controllers/product_list_controller_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class ProductListControllerTest < ActionController::TestCase
 	# TODO why does this test fail?
-  # test "should get list" do
-  #   get :list
-  #   assert_response :success
-  # end
+	test "should get list" do
+		get :list
+		assert_response :success
+	end
 
 end
