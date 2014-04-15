ce172da130000ca99e44ec03f38691ad2c528dc2
StevenHH <stevenhh@brandeis.edu>
2014-04-15 13:36:31 -0400

    migrations file for createratings had a mispelling, caused an error when trying to rerun migrations

--- db/migrate/20140414182439_create_ratings.rb
+++ db/migrate/20140414182439_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-      t.references :productra, index: true
+      t.references :product, index: true
       t.references :user, index: true
       t.integer :score, default: 0
 
