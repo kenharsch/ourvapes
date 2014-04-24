ac0d68a022e5e3527662c8d422bf581d897f9ee3
StevenHH <stevenhh@brandeis.edu>
2014-04-24 12:06:38 -0400

    added onclick function via jquery for filter type radio buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,18 @@
 //= require jquery_ujs
 //= require jquery.raty.min.js
 //= require jquery.ui.all
+
+$(document).on('ready page:load', function() {
+	var types = ["mouthpiece", "tank", "wick", "button", "battery", "charger"];
+	for (var i = 0; i < types.length; i++) {
+		$("#prod_type_"+types[i]).on("click",function() {
+			var type = String(this.id).charAt(10).toUpperCase()+String(this.id).slice(11);
+			window.location = "?type="+type;
+		});
+	}
+});
+$(document).on('ready page:load', function() {
+	$("#prod_type").on("click",function() {
+		window.location = "?type=";
+	});
+});
