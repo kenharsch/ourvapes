967ad2679ebd19af9693a14cceb01ba2603b8a03
StevenHH <stevenhh@brandeis.edu>
2014-04-24 00:53:32 -0400

    learning js--testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,12 @@
 //= require jquery_ujs
 //= require jquery.raty.min.js
 //= require jquery.ui.all
+
+$(document).ready(function() {
+	var types = ["all_types", "mouthpiece", "tank", "wick", "button", "battery", "charger"];
+	for (var i = 0; i < types.length; i++) {
+		$("#prod_type_"+types[i]).on("click",function() {
+			window.location.href = "http://stackoverflow.com";
+		});
+	}
+});
