70c1ab775df001835aa69556bdf21d3a9cd15482
Ken Harsch <kharsch@brandeis.edu>
2014-08-02 12:16:37 -0400

    solr config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Src::Application.configure do
 
   # tells Solr where to run / find the server (needed for Heroku)
   # Sunspot.config.solr.url = ENV['WEBSOLR_URL']
-  Sunspot.config.solr.url = 'http://index.websolr.com/solr/c4b294e7ed4'
+  Sunspot.config.solr.url = 'http://ec2-europe-west-ireland.opensolr.com/solr/ourvapes'
 end
